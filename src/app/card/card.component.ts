d1d694e97f8062a76b400ff6a14b3e87f14883a7
Andrey Novikov <NovikovAA.it@relef.ru>
2021-01-12 23:37:19 +0300

    add ngOnInit

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,6 +10,7 @@ export class CardComponent implements OnInit {
   number: number = 42
   title: string = 'Card component'
   text: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, eum?'
+  imgUrl: string = 'https://images.unsplash.com/photo-1585159904350-24ea5cf707c0?&auto=format&fit=crop&w=1950&q=80'
   array: number[] = [1, 1, 2, 3, 5, 8, 13]
   obj: object = {
     name: 'Vladilen',
@@ -22,6 +23,13 @@ export class CardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.imgUrl = 'https://images.unsplash.com/photo-1518409274682-1cb2fe2955a8?&auto=format&fit=crop&w=1568&q=80'
+    }, 3000)
+  }
+
+  getInfo(): string {
+    return 'This is my info'
   }
 
 }
