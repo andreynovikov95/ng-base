8ce2d9a16c69433eaed97e804fceea3a3ff1def5
Andrey Novikov <NovikovAA.it@relef.ru>
2021-01-12 23:05:34 +0300

    create types

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,9 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.styl']
+  styleUrls: ['./card.component.styl'],
+  interpolation: ['[[', ']]']
 })
 export class CardComponent implements OnInit {
+  number: number = 42
+  title: string = 'Card component'
+  text: string = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, eum?'
+  array: number[] = [1, 1, 2, 3, 5, 8, 13]
+  obj: object = {
+    name: 'Vladilen',
+    info: {
+      age: 25,
+      job: 'Frontend'
+    }
+  }
 
   constructor() { }
 
